c282aeeebf4812d1fd8b56f49f83a3c69759c163
arpruss <arpruss@gmail.com>
2016-07-26 10:11:49 -0500

    foo

--- resize.sh
+++ resize.sh
@@ -7,3 +7,5 @@ convert myicon.png -resize 144x144 res/drawable-xxhdpi/icon.png
 convert myicon.png -resize 96x96 res/drawable-xhdpi/icon.png
 convert myicon.png -resize 72x72 res/drawable-hdpi/icon.png
 convert myicon.png -resize 48x48 res/drawable-mdpi/icon.png
+convert myicon.png -resize 144x144 myicon144.png
+convert myicon.png -resize 512x512 myicon512.png
